d907e5c02f47816aa4d9cc93b3705f1cc9d0f7b9
Stanley Sandjo <r0878901@student.thomasmore.be>
2025-11-23 22:07:39 +0100

    Solved - Python - Two Sum

--- Easy/Two Sum/Python/solution.py
+++ Easy/Two Sum/Python/solution.py
@@ -3,15 +3,10 @@ from typing import List
 
 class Solution:
 	def twoSum(self, nums: List[int], target: int) -> List[int]:
-		list.sort(nums)
-		
-		left = 0
-		right = len(nums) - 1
+		searching = {}
+		for index, num in enumerate(nums):
+			if num in searching:
+				return [searching[num], index]
+			searching[target - num] = index
 
-		while True:
-			if target > nums[left] + nums[right]:
-				left += 1
-			elif target < nums[left] + nums[right]:
-				right -= 1
-			else:
-				return [left, right]
+		return []
