817e6c0b92de7235506a1b63be4037630328c643
Stanley Sandjo <r0878901@student.thomasmore.be>
2025-11-25 22:36:01 +0100

    Solved - Python - Longest Consecutive Sequence

--- Medium/Longest Consecutive Sequence/Python/solution.py
+++ Medium/Longest Consecutive Sequence/Python/solution.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
 	def longestConsecutive(self, nums: List[int]) -> int:
-		return 1
+		seen = set(nums)
+		max_streak = 0
+
+		for num in seen:
+			streak = 0
+			if num - 1 not in seen:
+				while num in seen:
+					streak += 1
+					num += 1
+				max_streak = max(streak, max_streak)
+		
+		return max_streak
