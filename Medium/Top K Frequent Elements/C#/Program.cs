15e8eee251922b9cc09652bf4bd2295eff598351
Stanley Ivan Sandjo <heboivan@gmail.com>
2026-02-01 22:53:52 +0100

    Added new approach - Solved - C# - Top K Frequent Elements

--- Medium/Top K Frequent Elements/C#/Program.cs
+++ Medium/Top K Frequent Elements/C#/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Top_K_Frequent_Elements;
 
 Solution task = new();
+
 Console.WriteLine(string.Join(", ", task.TopKFrequent([1,1,1,2,2,3], 2)));
 Console.WriteLine(string.Join(", ", task.TopKFrequent([1], 1)));
+
 Console.WriteLine(string.Join(", ", task.TopKFrequent_Buckets([1,1,1,2,2,3], 2)));
 Console.WriteLine(string.Join(", ", task.TopKFrequent_Buckets([1], 1)));
+
+Console.WriteLine(string.Join(", ", task.TopKFrequent_Queue([1,1,1,2,2,3], 2)));
+Console.WriteLine(string.Join(", ", task.TopKFrequent_Queue([1], 1)));
--- Medium/Top K Frequent Elements/C#/Solution.cs
+++ Medium/Top K Frequent Elements/C#/Solution.cs
@@ -1,52 +1,93 @@
-﻿namespace Top_K_Frequent_Elements;
+﻿using System;
 
+namespace Top_K_Frequent_Elements;
 
 public class Solution {
-    public int[] TopKFrequent(int[] nums, int k) {
-        PriorityQueue<int, int> frequency = new();
-        Dictionary<int, int> countMap = [];
+	public int[] TopKFrequent(int[] nums, int k) {
+		//? Bucket approach #1
+		Dictionary<int, int> freq = [];
+		List<HashSet<int>> freqGroups = [];
 
-        foreach (int num in nums) {
-            if (countMap.TryGetValue(num, out int count)) {
-                countMap[num] = ++count;
-            }
-            else countMap.Add(num, 1);
-        }
+		foreach (int n in nums) {
+			if (freq.TryGetValue(n, out int count)) {
+				freqGroups[count - 1].Remove(n);
+				freq[n] = ++count;
+			}
+			else {
+				count = 1;
+				freq.Add(n, count);
+			}
 
-        foreach ((int num, int count) in countMap) {
-            frequency.Enqueue(num, count);
-            if (frequency.Count > k) frequency.Dequeue();
-        }
+			if (count <= freqGroups.Count) 
+				freqGroups[count - 1].Add(n);
+			else
+				freqGroups.Add(new([n]));
+		}
 
-        int[] topK = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            topK[i] = frequency.Dequeue();
-        }
+		var ans = new int[k]; 
+		int idx = 0;
 
-        return topK;
-    }
-    public int[] TopKFrequent_Buckets(int[] nums, int k) {
-        int n = nums.Length;
-        Dictionary<int, int> countMap = [];
-        List<int>[] rows = new List<int>[n + 1];
+		for (int i = 1; i <= nums.Length; i++) {
+			foreach (int n in freqGroups[^i]) {
+				ans[idx++] = n;
 
-        foreach (int num in nums) {
-            if (countMap.TryGetValue(num, out int count)) {
-                countMap[num] = ++count;
-            }
-            else countMap[num] = 1;
-        }
+				if (idx == k)
+					return ans;
+			}
+		}
 
-        foreach ((int num, int count) in countMap) {
-            if (rows[count] == null) rows[count] = [num];
-            else rows[count].Add(num);
-        }
-    
-        List<int> topK = [];
-        for (int i = n; i >= 0 && topK.Count < k; i--) {
-            if (rows[i] != null) topK.AddRange(rows[i]);
-        }
+		return ans;
+	}
 
-        return [..topK];
-    }
+	public int[] TopKFrequent_Buckets(int[] nums, int k) {
+		//? Bucket approach #2
+		int n = nums.Length;
+		Dictionary<int, int> countMap = [];
+		List<int>[] rows = new List<int>[n + 1];
+
+		foreach (int num in nums) {
+				if (countMap.TryGetValue(num, out int count)) {
+					countMap[num] = ++count;
+				}
+				else countMap[num] = 1;
+		}
+
+		foreach ((int num, int count) in countMap) {
+				if (rows[count] == null) rows[count] = [num];
+				else rows[count].Add(num);
+		}
+	
+		List<int> topK = [];
+		for (int i = n; i >= 0 && topK.Count < k; i--) {
+				if (rows[i] != null) topK.AddRange(rows[i]);
+		}
+
+		return [..topK];
+	}
+
+	public int[] TopKFrequent_Queue(int[] nums, int k) {
+		//? Priority queue approach
+		PriorityQueue<int, int> freq = new();
+		Dictionary<int, int> countMap = [];
+
+		foreach (int num in nums) {
+				if (countMap.TryGetValue(num, out int count))
+					countMap[num] = ++count;
+				else 
+					countMap.Add(num, 1);
+		}
+
+		foreach ((int num, int count) in countMap) {
+				freq.Enqueue(num, count);
+				if (freq.Count > k) 
+					freq.Dequeue();
+		}
+
+		var topK = new int[k];
+		for (int i = k - 1; i >= 0; i--)
+				topK[i] = freq.Dequeue();
+
+		return topK;
+	}
+
 }
